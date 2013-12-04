6cceb66c37c9b5b76f1961dd691e2964d8990f3c
moznion <moznion@gmail.com>
2013-12-04 16:51:23 +0900

    Implement the basic test method

--- cpanfile
+++ cpanfile
@@ -1,6 +1,14 @@
-requires 'perl', '5.008001';
+requires 'Pod::Simple::Methody';
+requires 'Test::Builder::Module';
+requires 'parent';
+requires 'perl', '5.008005';
 
-on 'test' => sub {
-    requires 'Test::More', '0.98';
+on configure => sub {
+    requires 'CPAN::Meta';
+    requires 'CPAN::Meta::Prereqs';
+    requires 'Module::Build';
 };
 
+on test => sub {
+    requires 'Test::More', '0.98';
+};
--- lib/Test/Synopsis/Detail.pm
+++ lib/Test/Synopsis/Detail.pm
@@ -2,10 +2,30 @@ package Test::Synopsis::Detail;
 use 5.008005;
 use strict;
 use warnings;
+use parent qw/Test::Builder::Module/;
+use Test::Synopsis::Detail::Pod;
 
 our $VERSION = "0.01";
+our @EXPORT  = qw/synopsis_ok/;
 
+sub synopsis_ok {
+    my ($file) = @_;
+    return _synopsis_ok(__PACKAGE__->builder, $file);
+}
 
+sub _synopsis_ok {
+    my ($builder, $file) = @_;
+
+    local $Test::Builder::Level = $Test::Builder::Level + 1;
+
+    my $parser = Test::Synopsis::Detail::Pod->new;
+    $parser->parse_file($file);
+
+    my $got      = eval $parser->{target_code}; ## no critic
+    my $expected = eval $parser->{expected}; ## no critic
+
+    $builder->is_eq($got, $expected);
+}
 
 1;
 __END__
