ec53321a2ef14aaa28ebd73432f2e26c8713f6ee
moznion <moznion@gmail.com>
2013-12-05 19:34:09 +0900

    Support `# =>` notation.
    And goodbye `=for`...

--- lib/Test/Synopsis/Detail.pm
+++ lib/Test/Synopsis/Detail.pm
@@ -3,6 +3,8 @@ use 5.008005;
 use strict;
 use warnings;
 use parent qw/Test::Builder::Module/;
+use Compiler::Lexer;
+use Test::More ();
 use Test::Synopsis::Detail::Pod;
 
 our $VERSION = "0.01";
@@ -21,10 +23,56 @@ sub _synopsis_ok {
     my $parser = Test::Synopsis::Detail::Pod->new;
     $parser->parse_file($file);
 
-    my $got      = eval $parser->{target_code}; ## no critic
-    my $expected = eval $parser->{expected}; ## no critic
+    my $lexer  = Compiler::Lexer->new({verbose => 1});
+    my $tokens = $lexer->tokenize($parser->{target_code});
+    my $expectations = _analyze_expectations($tokens);
 
-    $builder->is_eq($got, $expected);
+    for my $expectation (@$expectations) {
+        my $got      = eval $expectation->{code};     ## no critic
+        my $expected = eval $expectation->{expected}; ## no critic
+
+        my $method = $expectation->{method};
+        if ($method eq 'is') {
+            Test::More::is($got, $expected);
+        }
+    }
+}
+
+sub _analyze_expectations {
+    my ($tokens) = @_;
+
+    my $line = 0;
+    my $code = '';    # target code for test
+    my @expectations; # store expectations for test
+    foreach my $token (@$tokens) {
+        if ($token->{name} eq 'Comment') {
+            # Not accept if it isn't on the same line as others
+            next if $token->{line} > $line;
+
+            # Accept special comment for this module
+            # e.g.
+            #     # => is 42
+            my ($expectation) = $token->{data} =~ /#\s*=>\s*(.+)/;
+            next unless $expectation;
+
+            # Accept test methods as string
+            my $method;
+            if ($expectation =~ s/^(is|isa|is_deeply|like)\s// && $1) {
+                $method = $1;
+            }
+
+            push @expectations, +{
+                'method'   => $method || 'is',
+                'expected' => $expectation,
+                'code'     => $code,
+            };
+        } else {
+            $code .= "$token->{data} "; # Stack code without comment
+            $line = $token->{line};
+        }
+    }
+
+    return \@expectations;
 }
 
 1;
--- lib/Test/Synopsis/Detail/Pod.pm
+++ lib/Test/Synopsis/Detail/Pod.pm
@@ -6,17 +6,12 @@ sub new {
     my $class  = shift;
     my $parser = $class->SUPER::new(@_);
 
-    # Accept `test-synopsis-detail` format at formatter (=begin, =end, =for)
-    $parser->accept_targets(qw/test-synopsis-detail/);
-
     # NOTE I think not good way...
     $parser->{in_head1}    = 0;
     $parser->{in_synopsis} = 0;
     $parser->{in_verbatim} = 0;
-    $parser->{in_test}     = 0;
 
     $parser->{target_code} = '';
-    $parser->{expected}    = '';
 
     return $parser;
 }
@@ -30,11 +25,6 @@ sub handle_text {
     # Target codes (that is synopsis code)
     if ($self->{in_synopsis} && $self->{in_verbatim}) {
         $self->{target_code} = $text;
-    }
-
-    # Extract expected value
-    if ($self->{in_synopsis} && $self->{in_test}) {
-        $self->{expected} = $text;
     }
 }
 
@@ -61,19 +51,5 @@ sub end_Verbatim {
     my($self) = @_;
 
     $self->{in_verbatim} = 0;
-}
-
-# For `$self->{in_test}`.
-# Only `=for` that is labeled `test-synopsis-detail` comes here.
-sub start_for {
-    my($self, $attrs) = @_;
-
-    $self->{in_test} = 1;
-}
-
-sub end_for {
-    my($self) = @_;
-
-    $self->{in_test} = 0;
 }
 1;
